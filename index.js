dff0618cf8a49bea1f64eb1cb95fd5a70a4caac3
shacchin <sw20fd3s.ryota@gmail.com>
2020-10-01 22:13:34 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const showParam = (id, x, y) => {
+  const element = document.getElementById(`${id}`);
+  // const newContent = document.createTextNode(`(${x}, ${y})`);
+  // element.insertBefore(newContent, element.firstChild);
+  element.innerText = `(${x}, ${y})`;
+};
+
+const handleClick = (e) => {
+  showParam("offset-param", e.offsetX, e.offsetY);
+  showParam("client-param", e.clientX, e.clientY);
+  showParam("page-param", e.pageX, e.pageY);
+  showParam("screen-param", e.screenX, e.screenY);
+};
+
+const clickableElement = document.getElementsByClassName("click-area")[0];
+if (clickableElement) {
+  console.log("ok!");
+  clickableElement.addEventListener("click", handleClick, false);
+  clickableElement.addEventListener("tourch", handleClick, false);
+}
